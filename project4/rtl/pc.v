e37feb098973245b53c2b7fcbc262fc6977a7bdb
JackMertes <jacksonmertes@icloud.com>
2025-10-19 00:55:35 -0500

    Proper reset for pc

--- project4/rtl/pc.v
+++ project4/rtl/pc.v
@@ -1,15 +1,24 @@
-module pc (
-	//global clock signal
-	input wire clk,
+module pc #(
+    parameter RESET_ADDR = 32'h00000000
+)(
+    input wire clk,
+	//active high, synchronous reset
+    input wire i_rst,
 	//the value of the pc at the next clock cycle
 	input wire [31:0] i_pc,
 	//the current value of the pc
-	output reg [31:0] o_pc,
-	);
-	
-	always @(posedge clk) begin
-		o_pc <= i_pc;
-	end
-	
+    output reg [31:0] o_pc
+);
+
+    always @(posedge clk) begin
+        if (i_rst)
+            o_pc <= RESET_ADDR;
+        else
+            o_pc <= i_pc;
+    end
 
 endmodule
+
+
+
+
